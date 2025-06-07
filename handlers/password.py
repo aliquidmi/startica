62a97a5087d0a19b3f088150a306157692b5698e
arefloid <arefloid@gmail.com>
2025-06-07 11:47:16 +0300

    password with loggers and buttons

--- handlers/password.py
+++ handlers/password.py
@@ -1,7 +1,12 @@
 import random
 import string
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ContextTypes
+import logging
+from keyboards import get_navigation_keyboard, get_main_keyboard
+
+# Ініціалізуємо лог
+logger = logging.getLogger(name)
 
 # Генерація пароля
 def generate_password() -> str:
@@ -13,11 +18,29 @@ def generate_password() -> str:
 # Обробник команди /password
 async def password_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     try:
-        password = generate_password()
-        await update.message.reply_text(
-            f"Ось ваш випадковий пароль:\n\n`{password}`",
-            parse_mode="Markdown"
-        )
+        text = update.message.text
+
+        if text == "/password" or text == "Ще раз":
+            password = generate_password()
+            keyboard = get_navigation_keyboard()
+            await update.message.reply_text(
+                f"Ось ваш випадковий пароль:\n\n`{password}`",
+                parse_mode="Markdown"
+            )
+            logger.info("Пароль згенеровано і надіслано користувачу.")
+
+        elif text == "Назад":
+            keyboard = get_main_keyboard()
+            await update.message.reply_text("Повернулись у головне меню.", reply_markup=keyboard)
+            logger.info(f"Користувач повернувся у головне меню.")
+
+        else:
+            await update.message.reply_text(
+                "Введи /password або натисни кнопку.",
+                reply_markup=get_navigation_keyboard()
+            )
+            logger.warning(f"Користувач надіслав невідому команду: {text}")
+
     except Exception as e:
+        logger.error(f"Помилка в password_handler: {e}")
         await update.message.reply_text("Виникла помилка при генерації пароля.")
-        print(f"[ERROR /password]: {e}")
